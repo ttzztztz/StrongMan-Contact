3f4da90ff268f9de4ed1a9bf918f834423a9336c
ttzztztz <ttzztztz@outlook.com>
2020-03-27 16:12:42 +0800

    Initial Commit

---
+++ StrongMan-Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrongMan-Contact
+//
+//  Created by 杨子越 on 2020/3/27.
+//  Copyright © 2020 Rabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
